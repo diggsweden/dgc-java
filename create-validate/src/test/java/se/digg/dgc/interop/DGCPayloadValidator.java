dd24801ed67e15e59e80eb1e770422b66b9ca321
Martin LindstrÃ¶m <martin.lindstrom@litsec.se>
2021-05-27 11:03:55 +0200

    Can now be built with and for Java 8
    Use the maven profile j8-build
    
    For example: mvn clean install -P!default,j8-build

--- create-validate/src/test/java/se/digg/dgc/interop/DGCPayloadValidator.java
+++ create-validate/src/test/java/se/digg/dgc/interop/DGCPayloadValidator.java
@@ -1,17 +1,7 @@
 /*
- * Copyright 2021 Litsec AB
+ * MIT License
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2021 Myndigheten för digital förvaltning (DIGG)
  */
 package se.digg.dgc.interop;
 
@@ -347,7 +337,7 @@ public class DGCPayloadValidator {
     if (v == null) {
       return;
     }
-    if (v.isBlank()) {
+    if (v.trim().isEmpty()) {
       return;
     }
     if (valueSet.getValue(v) == null) {
@@ -366,7 +356,7 @@ public class DGCPayloadValidator {
       errors.add(Report.error(item, "Field is not a string"));
     }
     final String s = obj.AsString();
-    if (s.isBlank()) {
+    if (s.trim().isEmpty()) {
       if (required) {
         errors.add(Report.error(item, "Field is required but is empty, or contains only blanks"));
       }
