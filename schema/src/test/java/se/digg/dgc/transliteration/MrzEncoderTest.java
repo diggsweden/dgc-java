11e2efc66abba23194609e03ef51e32588cbf3fa
Martin LindstrÃ¶m <martin.lindstrom@litsec.se>
2021-06-09 16:54:09 +0200

    Truncation for MRZ-encoder

--- schema/src/test/java/se/digg/dgc/transliteration/MrzEncoderTest.java
+++ schema/src/test/java/se/digg/dgc/transliteration/MrzEncoderTest.java
@@ -19,10 +19,22 @@ public class MrzEncoderTest {
 
   @Test
   public void testMrzEncoder() throws Exception {    
+        
     Assert.assertEquals("LINDSTROEM", MrzEncoder.encode("Lindström"));
     Assert.assertEquals("KARL<AAKE", MrzEncoder.encode("Karl Åke"));
     Assert.assertEquals("KARL<AAKE", MrzEncoder.encode("Karl-Åke"));
     Assert.assertEquals("ANDRE", MrzEncoder.encode("André"));
     Assert.assertEquals("OLEARY", MrzEncoder.encode("O'Leary"));
+    Assert.assertEquals("AERGE", MrzEncoder.encode("Àérge"));
+    Assert.assertEquals("BOESSE", MrzEncoder.encode("BŒße"));
+    Assert.assertEquals("BOESSE", MrzEncoder.encode("Bœße"));
+    Assert.assertEquals("DAN", MrzEncoder.encode("Ðan"));
+    Assert.assertEquals("ZLATAN", MrzEncoder.encode("Žlatan"));
+  }
+  
+  @Test
+  public void testTruncating() throws Exception {
+    Assert.assertEquals("CHAYAPA<DEJTHAMRONG<KRASUANG<NILAVADHANANANDA<WARRANDYTE<WARNAMBOOL<PAPANDROPOUL", 
+      MrzEncoder.encode("Chayapa Dejthamrong Krasuang Nilavadhanananda Warrandyte Warnambool Papandropoulous"));
   }
 }
